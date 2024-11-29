651eb5b17f23b41a6ea436c0060727f4c5fe0b5f
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-11-30 06:19:13 +0900

    Add Mt19937ar.make and make_self_init

--- source/mt19937ar.ml
+++ source/mt19937ar.ml
@@ -1,8 +1,13 @@
+external random_seed: unit -> int array = "caml_sys_random_seed";;
+
 type t;;
 
 external make_int32: int32 -> t = "mlmt_mt19937ar_make_int32";;
 external make_int32_array: int32 array -> t =
 	"mlmt_mt19937ar_make_int32_array";;
+
+let make (seed: int array) = make_int32_array (Array.map Int32.of_int seed);;
+let make_self_init () = make (random_seed ());;
 
 external bits31: t -> int = "mlmt_mt19937ar_bits31";;
 external bits32: t -> int32 = "mlmt_mt19937ar_bits32";;
--- source/mt19937ar.mli
+++ source/mt19937ar.mli
@@ -2,6 +2,8 @@ type t
 
 external make_int32: int32 -> t = "mlmt_mt19937ar_make_int32"
 external make_int32_array: int32 array -> t = "mlmt_mt19937ar_make_int32_array"
+val make: int array -> t
+val make_self_init: unit -> t
 
 external bits31: t -> int = "mlmt_mt19937ar_bits31"
 external bits32: t -> int32 = "mlmt_mt19937ar_bits32"
