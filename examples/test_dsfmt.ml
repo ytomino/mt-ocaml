342b44a3b2f395180dd2521d768a8d07571cffb3
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-12-23 08:02:39 +0900

    Add Dsfmt_*.bits52
    They do not provide bits32 by dsfmt_genrand_uint32 because for
    consistency with the other functions to take from higher bits.

--- examples/test_dsfmt.ml
+++ examples/test_dsfmt.ml
@@ -3,6 +3,7 @@ module type DsfmtS = sig
 	type t
 	val make_int32: int32 -> t
 	val copy: t -> t
+	val bits52: t -> int64
 	val float_bits52: t -> float
 end;;
 
@@ -15,6 +16,17 @@ module Check (Dsfmt: DsfmtS) = struct
 	for _ = 1 to Dsfmt.min_float_array_length + 1 do
 		let r = Dsfmt.float_bits52 s1 in
 		assert (Dsfmt.float_bits52 s2 = r)
+	done;;
+	
+	(* taking from higher bits *)
+	
+	let s1 = Dsfmt.make_int32 1234l in
+	let s2 = Dsfmt.copy s1 in
+	(* bits52 *)
+	for _ = 1 to 10 do
+		let x1 = Int64.of_float (ldexp (Dsfmt.float_bits52 s1) 52) in
+		let x2 = Dsfmt.bits52 s2 in
+		assert (x1 = x2)
 	done;;
 	
 end;;
