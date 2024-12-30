9c4c119e1b93131776317a6fee4da80d0d45206f
ytomino <demoonlit@panathenaia.halfmoon.jp>
2024-12-30 22:45:48 +0900

    Write comments for ocamldoc

--- source/mt19937ar.mli
+++ source/mt19937ar.mli
@@ -1,4 +1,12 @@
+(** Mersenne Twister pseudorandom number generator, with the period 2^19937 - 1
+    in 32 bit units. *)
+
+(** {1 Generator} *)
+
 type t
+(** The type of the state. *)
+
+(** {2 Initialization} *)
 
 external make_int32: int32 -> t = "mlmt_mt19937ar_make_int32"
 external make_int32_array: int32 array -> t = "mlmt_mt19937ar_make_int32_array"
@@ -7,21 +15,49 @@ val make_self_init: unit -> t
 
 external copy: t -> t = "mlmt_mt19937ar_copy"
 
+(** {2 Bit generation} *)
+
 external bits31: t -> int = "mlmt_mt19937ar_bits31" [@@ocaml.noalloc]
+(** Draws out 31 bits of higher part of 32 bits.
+    It calls {e genrand_int31}. *)
+
 external bits32: t -> (int32 [@ocaml.unboxed]) =
 	"mlmt_mt19937ar_bits32" "mlmt_mt19937ar_bits32_unboxed"
 	[@@ocaml.noalloc]
+(** Draws out 32 bits.
+    It calls {e genrand_int32}. *)
+
 val bits64: t -> int64
+(** Draws out 64 bits.
+    The result is composed by drawing out 32 bit twice, first for the lower
+    bits and then for upper bits, with {!bits32}. *)
+
 external float_bits32: t -> (float [@ocaml.unboxed]) =
 	"mlmt_mt19937ar_float_bits32" "mlmt_mt19937ar_float_bits32_unboxed"
 	[@@ocaml.noalloc]
+(** Draws out 32 bits as [float] \[0,1).
+    It calls {e genrand_real2}. *)
+
+(** {2 Uniform distribution} *)
 
 val int: t -> int -> int
+(** \[0,[bound]).
+    Draws out values repeatedly as needed in 32 bit units if the [bound] fits
+    into 32 bits otherwise 64 bit units. *)
+
 val int32: t -> int32 -> int32
 val int64: t -> int64 -> int64
 val nativeint: t -> nativeint -> nativeint
-val float: t -> float -> float (* [0,bound), unlike Stdlib.Random.float *)
+
+val float: t -> float -> float
+(** \[0,[bound]), unlike [Stdlib.Random.float].
+    Draws out 64 bits once. *)
+
 val bool: t -> bool
+(** [false] or [true].
+    Draws out 32 bits once. *)
+
+(** {2 Serialization} *)
 
 val import: int32 array * int -> t
 external export: t -> int32 array * int = "mlmt_mt19937ar_export"
--- source/uniform_distribution.mli
+++ source/uniform_distribution.mli
@@ -1,27 +1,82 @@
+(** Converting from the generated bit sequence to the ranged number with a
+    uniform distribution. *)
+
+(** {1 In 32 bit units} *)
+
+(** {2 Normal functions} *)
+
 val int: bits32:('a -> int32) -> bits64:('a -> int64) -> 'a -> int -> int
+(** \[0,[bound]).
+    Draws out values repeatedly as needed by [bits32] if the [bound] fits into
+    32 bits otherwise [bits64]. *)
+
 val int32: bits32:('a -> int32) -> 'a -> int32 -> int32
 val int64: bits32:('a -> int32) -> bits64:('a -> int64) -> 'a -> int64 -> int64
 
+(** {2 Pre-bindable functions}
+    For multiple times to call these functions with the same parameters, it can
+    make more efficient functions by partial applying the parameters to do part
+    of the needed calculation before the calls.
+    For example: {[
+	let f = bind_int ~bits32 ~bits64 bound in
+	List.init times (fun _ -> f state ())]}
+    is better than: {[
+	List.init times (fun _ -> int ~bits32 ~bits64 state bound)]} *)
+
 val bind_int: bits32:('a -> int32) -> bits64:('a -> int64) -> int -> 'a ->
 	unit -> int
+(** Like {!int} except the order of the parameters differs for efficiency of
+    partial application. *)
+
 val bind_int32: bits32:('a -> int32) -> int32 -> 'a -> unit -> int32
 val bind_int64: bits32:('a -> int32) -> bits64:('a -> int64) -> int64 -> 'a ->
 	unit -> int64
 
+(** {1 In any bit units.} *)
+
+(** {2 Normal functions} *)
+
 val int_from_int64_bits: width:int -> bits:('a -> int64) -> 'a -> int -> int
+(** \[0,[bound]).
+    Draws out values repeatedly as needed by [bits] in [width] bit units.
+    [bound] must be fits into [width] bits, and [bits] must return a value fits
+    into [width] bits. *)
+
 val int32_from_int64_bits: width:int -> bits:('a -> int64) -> 'a -> int32 ->
 	int32
 val int64_from_int64_bits: width:int -> bits:('a -> int64) -> 'a -> int64 ->
 	int64
+
 val float_from_int64_bits: width:int -> bits:('a -> int64) -> 'a -> float ->
-	float (* [0,bound) *)
+	float
+(** \[0,[bound]).
+    Draws out [width] bits once by [bits].
+    [bits] must return a value fits into [width] bits.
+    The result has the precision of the minimum of 53 bits and [width] bits. *)
+
 val float_exclusive_from_int64_bits: width:int -> bits:('a -> int64) -> 'a ->
-	float -> float (* (0,bound) *)
+	float -> float
+(** (0,[bound]).
+    Draws out values repeatedly as needed by [bits] in [width] bit units.
+    [width] must be at least 2 and [bits] must return a value fits into [width]
+    bits.
+    The result has the precision of the minimum of 53 bits and [width] bits. *)
+
 val float_inclusive_from_int64_bits: width:int -> bits:('a -> int64) -> 'a ->
-	float -> float (* [0,bound] *)
+	float -> float
+(** \[0,[bound]\].
+    Draws out values repeatedly as needed by [bits] in [width] bit units.
+    [bits] must return a value fits into [width] bits.
+    The result has the precision of the minimum of 53 bits and [width] - 1
+    bits. *)
+
+(** {2 Pre-bindable functions} *)
 
 val bind_int_from_int64_bits: width:int -> bits:('a -> int64) -> int -> 'a ->
 	unit -> int
+(** Like {!int_from_int64_bits} except the order of the parameters differs for
+    efficiency of partial application. *)
+
 val bind_int32_from_int64_bits: width:int -> bits:('a -> int64) -> int32 ->
 	'a -> unit -> int32
 val bind_int64_from_int64_bits: width:int -> bits:('a -> int64) -> int64 ->
